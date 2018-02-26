e42e95d775e4f15c2870418e947a583b15fda8d9
cristinamarin22 <cristinamarin2201@gmail.com>
2018-02-26 13:01:46 +0200

    Added FilterTemperatureHumidityClass

---
+++ SmartHouse/Models/FilterTemperatureHumidityClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHouse.Models
+{
+    public partial class FilterTemperatureHumidityClass
+    {
+        public float? TemperatureMinValue { get; set; }
+        public float? TemperatureMaxValue { get; set; }
+        public float? HumidityMinValue { get; set; }
+        public float? HumidityMaxValue { get; set; }
+        public DateTime? DateMinValue { get; set; }
+        public DateTime? DateMaxValue { get; set; }
+    }
+}
